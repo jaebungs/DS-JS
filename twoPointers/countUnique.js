64f61ddbebe7b1c36783dfd6fcec734db65b5c12
Jae Park <jaep@criticalmass.com>
2021-07-14 21:17:25 -0400

    update test

--- twoPointers/countUnique.js
+++ twoPointers/countUnique.js
@@ -2,6 +2,7 @@ function countUniqueValue(array) {
     let numberOfUnique = 0;
     let left = 0;
     let right = array.length - 1;
+    let omg = 'whyyy'
 
-    
+    console.log("tttttteststetstsetes")
 }
