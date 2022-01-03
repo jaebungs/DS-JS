6bb92150b7df036d31f88f151c51cd5cf23c532b
Jae Park <jaep@criticalmass.com>
2022-01-03 11:14:28 -0500

    optimazed version

--- searchingAlgo/selectionSort/selectionSort.js
+++ searchingAlgo/selectionSort/selectionSort.js
@@ -3,23 +3,21 @@
 
 // pseudocode
 // - store the smallest value you've seen
-// - compare this value with other values
+// - compare the smallest value with value next to it
 // - update the minimum value if there is a value smaller
 // - If the minimum is not the index we started, swap the two value
 // repeat the process until the end of the array 
 
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
-        let minimum = arr[i];
-        let newMinIndex;
-        for (let j = i; j < arr.length; j++) {
-            if (arr[j] < minimum) {
-                minimum = arr[j];
-                newMinIndex = j
+        let minimum = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[i]) {
+                minimum = j
             }
         }
-        if (minimum !== arr[i] && newMinIndex) {
-            [ arr[i], arr[newMinIndex] ] = [ arr[newMinIndex], arr[i] ] //ES2015 array value swap
+        if (minimum !== i) {
+            [ arr[i], arr[minimum] ] = [ arr[minimum], arr[i] ] //ES2015 array value swap
         }
     }
 
