cfbfd5665c07c6e743a99e75ac18327a6504e736
Jae Park <jaebungs@gmail.com>
2022-01-17 22:58:22 -0500

    Single linkedList push, pop instance methods

--- singlyLinkedLIst/singlyLinkedList.js
+++ singlyLinkedLIst/singlyLinkedList.js
@@ -13,8 +13,8 @@ class Node {
     - Function accpet a value
     - Create a new node using the passed value
     - If not head, set head and tail.
-    - Otherwise set the next property on the tail to be the new node and set the tail property on the list
-    to be the newly created node.
+    - Otherwise set the next property on the tail to be the new node and 
+    set the tail property on the list to be the newly created node.
     - increment the length
 */
 class SinglyLinkedList {
@@ -24,10 +24,47 @@ class SinglyLinkedList {
         this.length = 0;
     }
 
+    push(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return
+        let current = this.head;
+        let temp = this.head;
+
+        while(current.next) {
+            temp = current
+            current = current.next
+        }
+        this.tail = temp
+        this.tail.next = null;
+        this.length--;
+        
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return current;
+    }
 }
 
 let linkedList = new SinglyLinkedList()
-SinglyLinkedList("Hi")
-SinglyLinkedList("My name is")
-SinglyLinkedList("Sasha")
+linkedList.push("Hi")
+linkedList.push("My name is")
+linkedList.push("Sasha")
+linkedList.pop()
 
+console.log(linkedList)
